9c7fad2035d96e281ea72321332551da9a4de1cc
Noa-Himesaka <64351595+Elaina-Alex@users.noreply.github.com>
2021-11-18 19:37:32 +0800

    Add files via upload

--- instance.py
+++ instance.py
@@ -1,5 +1,7 @@
 from config import *
-
+import os
+import re
+import time
 
 class Vars:
     cfg = Config('Config.json', os.getcwd())
@@ -15,7 +17,28 @@ def get(prompt, default=None):
         elif default is not None:
             return default
 
+def del_title(title):
+    """删去windowns不规范字符"""
+    return re.sub(r'[？?\*|“<>:/]', '', title)
+
 def content_(content):
-    import re
     return ''.join([re.sub(r'^\s*', "\n　　", content)
         for content in content.split("\n") if re.search(r'\S', content) != None])
+    
+def write(PATH, mode, info=None):
+    if info is not None:
+        try:
+            with open(PATH, f'{mode}', encoding='UTF-8', newline='') as file:
+                file.writelines(info)
+        except (UnicodeEncodeError, UnicodeDecodeError)as e:
+            print(e)
+            with open(PATH, f'{mode}', encoding='gbk', newline='') as file:
+                file.writelines(info)
+    else:
+        try:
+            file =  open(PATH, f'{mode}', encoding='UTF-8')
+            return file
+        except (UnicodeEncodeError, UnicodeDecodeError) as e:
+            print(e)
+            file =  open(PATH, f'{mode}', encoding='gbk')
+            return file
