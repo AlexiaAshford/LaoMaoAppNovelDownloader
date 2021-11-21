8dcfb3cd96a5bc779735d2aebb01e6f1c1a48002
Noa-Himesaka <64351595+Elaina-Alex@users.noreply.github.com>
2021-11-21 15:12:16 +0800

    Add files via upload

--- API/HttpUtil.py
+++ API/HttpUtil.py
@@ -10,15 +10,17 @@ headers = {
     "Connection": "Keep-Alive",
     "Cache-Control": "no-cache",
     "Accept-Encoding": "gzip",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
+    "User-Agent": "okhttp/3.3.0"
 }
+
+session = requests.Session()
 
 
 def get(url, params=None, retry=10, **kwargs):
     api_url = UrlConstants.WEB_SITE + url.replace(UrlConstants.WEB_SITE, '')
     for count in range(retry):
         try:
-            return example(decrypt(str(requests.get(api_url, params=params, headers=headers, **kwargs).text)))
+            return example(decrypt(str(session.get(api_url, params=params, headers=headers, **kwargs).text)))
         except (OSError, TimeoutError, IOError) as e:
             if retry != retry - 1:
                 print("\nGet Error Retry: " + str(e) + '\n' + api_url)
@@ -28,16 +30,16 @@ def get(url, params=None, retry=10, **kwargs):
                 sys.exit(1)
 
 
-
 def post(url, data=None, retry=10, **kwargs):
     api_url = UrlConstants.WEB_SITE + url.replace(UrlConstants.WEB_SITE, '')
     for count in range(retry):
         try:
-            return example(decrypt(str(requests.post(api_url, data, headers=headers, **kwargs).text)))
+            return example(decrypt(str(session.post(api_url, data, headers=headers, **kwargs).text)))
         except (OSError, TimeoutError, IOError) as e:
             if retry != retry - 1:
                 print("\nGet Error Retry: " + str(e) + '\n' + api_url)
                 time.sleep(1 * count)
             else:
-                print("\nPost Failed: " + str(e) + '\n' + api_url + "\nTerminating......")
+                print("\nPost Failed: " + str(e) + '\n' +
+                      api_url + "\nTerminating......")
                 sys.exit(1)
