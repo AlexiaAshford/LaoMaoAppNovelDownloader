1a6f9982b19fde84363349e6982d4c62336572c8
Noa-Himesaka <64351595+Elaina-Alex@users.noreply.github.com>
2021-11-22 21:11:27 +0800

    Add files via upload

--- instance.py
+++ instance.py
@@ -2,16 +2,33 @@ from config import *
 import os
 import re
 import time
+from rich import print
+
 
 class Vars:
     cfg = Config('Config.json', os.getcwd())
     current_bookshelf = None
     current_book = None
 
+
+def mkdir(file_path):
+    if not os.path.exists(file_path):
+        os.mkdir(file_path)
+        print(f'已创建{file_path}文件夹')
+    else:
+        pass
+
+
+def makedirs(file_path, file_name):
+    if not os.path.exists(os.path.join(file_path, file_name)):
+        os.makedirs(os.path.join(file_path, file_name))
+
+
 def time_(_time_):
     if type(_time_) is not int:
         _time_ = int(_time_)
     return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(_time_))
+
 
 def get(prompt, default=None):
     while True:
@@ -21,14 +38,17 @@ def get(prompt, default=None):
         elif default is not None:
             return default
 
+
 def del_title(title):
     """删去windowns不规范字符"""
     return re.sub(r'[？?\*|“<>:/\\]', '', title)
 
+
 def content_(content):
     return ''.join([re.sub(r'^\s*', "\n　　", content)
-        for content in content.split("\n") if re.search(r'\S', content) != None])
-    
+                    for content in content.split("\n") if re.search(r'\S', content) != None])
+
+
 def write(PATH, mode, info=None):
     if info is not None:
         try:
@@ -40,9 +60,9 @@ def write(PATH, mode, info=None):
                 file.writelines(info)
     else:
         try:
-            file =  open(PATH, f'{mode}', encoding='UTF-8')
+            file = open(PATH, f'{mode}', encoding='UTF-8')
             return file
         except (UnicodeEncodeError, UnicodeDecodeError) as e:
             print(e)
-            file =  open(PATH, f'{mode}', encoding='gbk')
+            file = open(PATH, f'{mode}', encoding='gbk')
             return file
